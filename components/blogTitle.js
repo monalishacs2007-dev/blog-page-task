0a326ffe44d369a0ff7d09ef09429e5f20b6b487
monalishacs2007-dev <monalisha.cs2007@gmail.com>
2026-07-01 11:04:43 +0530

    Update blogTitle.js

--- components/blogTitle.js
+++ components/blogTitle.js
@@ -1,1 +1,16 @@
 
+function renderBlogTitle(component, parent) {
+    const section = document.createElement("section");
+
+    section.className = "mb-8";
+
+    const title = document.createElement("h1");
+
+    title.className = "text-5xl font-bold text-gray-900";
+
+    title.textContent = component.data.title[0].value;
+
+    section.appendChild(title);
+
+    parent.appendChild(section);
+}
