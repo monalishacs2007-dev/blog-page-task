c6bf3961348277641792cc7e9c5e2e712a8d8c05
monalishacs2007-dev <monalisha.cs2007@gmail.com>
2026-07-01 11:09:07 +0530

    Implement blog description rendering function
    Added a function to render blog descriptions with images and formatted content.

--- components/blogDescription.js
+++ components/blogDescription.js
@@ -1,1 +1,33 @@
 
+function renderBlogDescription(component, parent) {
+
+    const section = document.createElement("section");
+
+    section.className = "bg-white rounded-xl shadow p-8 prose max-w-none mb-10";
+
+    component.data.description.forEach((item) => {
+
+        if (item.image) {
+            section.innerHTML += `
+                <img
+                    src="${item.image}"
+                    alt=""
+                    class="w-full rounded-lg mb-5">
+            `;
+        }
+
+        let html = item.content;
+
+        html = html.replace(/\*\*(.*?)\*\*/g, "<h2>$1</h2>");
+
+        html = html.replace(/\n/g, "<br>");
+
+        section.innerHTML += `
+            <div class="mb-8">
+                ${html}
+            </div>
+        `;
+    });
+
+    parent.appendChild(section);
+}
